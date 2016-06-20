4aba8265e6af0bd05e948f1df799f1204043a7c2
Megaju <julien_malle@outlook.fr>
2016-06-20 10:30:24 +0200

    bug nav vertical done

--- js/navbar-transparent.js
+++ js/navbar-transparent.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $('.fa-stethoscope').css('color', '#143143');
+                $('.fa-stethoscope').css('color', '#337ab7');
                 $('.fa-stethoscope').css('transition', '0.5s');
             } else {
                 $(".fa-stethoscope").css('color', '#F58900');
@@ -41,7 +41,7 @@ $(document).ready(function () {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $('.fa-camera').css('color', '#143143');
+                $('.fa-camera').css('color', '#337ab7');
                 $('.fa-camera').css('transition', '0.5s');
             } else {
                 $(".fa-camera").css('color', '#F58900');
@@ -58,7 +58,7 @@ $(document).ready(function () {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $('.fa-medkit').css('color', '#143143');
+                $('.fa-medkit').css('color', '#337ab7');
                 $('.fa-medkit').css('transition', '0.5s');
             } else {
                 $(".fa-medkit").css('color', '#F58900');
@@ -75,7 +75,7 @@ $(document).ready(function () {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $('.fa-heartbeat').css('color', '#143143');
+                $('.fa-heartbeat').css('color', '#337ab7');
                 $('.fa-heartbeat').css('transition', '0.5s');
             } else {
                 $(".fa-heartbeat").css('color', '#F58900');
@@ -92,11 +92,28 @@ $(document).ready(function () {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $('.fa-hospital-o').css('color', '#143143');
+                $('.fa-hospital-o').css('color', '#337ab7');
                 $('.fa-hospital-o').css('transition', '0.5s');
             } else {
                 $(".fa-hospital-o").css('color', '#F58900');
                 $('.fa-hospital-o').css('transition', '0.5s');
+            }
+        });
+    }
+});
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#nac');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-paw').css('color', '#337ab7');
+                $('.fa-paw').css('transition', '0.5s');
+            } else {
+                $(".fa-paw").css('color', '#F58900');
+                $('.fa-paw').css('transition', '0.5s');
             }
         });
     }
@@ -109,11 +126,11 @@ $(document).ready(function () {
         $(document).scroll(function () {
             scroll_start = $(this).scrollTop();
             if (scroll_start > offset.top) {
-                $('.fa-paw').css('color', '#143143');
-                $('.fa-paw').css('transition', '0.5s');
+                $('.fa-eur').css('color', '#337ab7');
+                $('.fa-eur').css('transition', '0.5s');
             } else {
-                $(".fa-paw").css('color', '#F58900');
-                $('.fa-paw').css('transition', '0.5s');
+                $(".fa-eur").css('color', '#F58900');
+                $('.fa-eur').css('transition', '0.5s');
             }
         });
     }
