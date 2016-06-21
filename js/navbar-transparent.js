1f9919c0c10bcc50d2a54ed21c6ad0a52eef933b
solenehuault <solene.huault@gmail.com>
2016-06-21 10:00:29 +0200

    ajout js navbar conseils

--- js/navbar-transparent.js
+++ js/navbar-transparent.js
@@ -1,3 +1,4 @@
+//animation navbar fixed (transparence)
 $(document).ready(function () {
     var scroll_start = 0;
     var startchange = $('#startchange');
@@ -17,7 +18,7 @@ $(document).ready(function () {
 });
 
 
-
+//animation navbar side prestations (changement de couleur)
 $(document).ready(function () {
     var scroll_start = 0;
     var startchange = $('#consultation');
@@ -138,3 +139,149 @@ $(document).ready(function () {
     }
 });
 
+
+
+//animation navbar side conseils (changement de couleur)
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#dent');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-user-md').css('color', '#337ab7');
+                $('.fa-user-md').css('transition', '0.5s');
+            } else {
+                $(".fa-user-md").css('color', '#F58900');
+                $('.fa-user-md').css('transition', '0.5s');
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#yeux-oreilles');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-eye').css('color', '#337ab7');
+                $('.fa-eye').css('transition', '0.5s');
+            } else {
+                $(".fa-eye").css('color', '#F58900');
+                $('.fa-eye').css('transition', '0.5s');
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#pelage');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-paw').css('color', '#337ab7');
+                $('.fa-paw').css('transition', '0.5s');
+            } else {
+                $(".fa-paw").css('color', '#F58900');
+                $('.fa-paw').css('transition', '0.5s');
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#parasite');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-bug').css('color', '#337ab7');
+                $('.fa-bug').css('transition', '0.5s');
+            } else {
+                $(".fa-bug").css('color', '#F58900');
+                $('.fa-bug').css('transition', '0.5s');
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#vaccin-chien');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-plus-square').css('color', '#337ab7');
+                $('.fa-plus-square').css('transition', '0.5s');
+            } else {
+                $(".fa-plus-square").css('color', '#F58900');
+                $('.fa-plus-square').css('transition', '0.5s');
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#vaccin-chat');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-medkit').css('color', '#337ab7');
+                $('.fa-medkit').css('transition', '0.5s');
+            } else {
+                $(".fa-medkit").css('color', '#F58900');
+                $('.fa-medkit').css('transition', '0.5s');
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#id');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-info-circle').css('color', '#337ab7');
+                $('.fa-info-circle').css('transition', '0.5s');
+            } else {
+                $(".fa-info-circle").css('color', '#F58900');
+                $('.fa-info-circle').css('transition', '0.5s');
+            }
+        });
+    }
+});
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#education');
+    var offset = startchange.offset();
+    if (startchange.length) {
+        $(document).scroll(function () {
+            scroll_start = $(this).scrollTop();
+            if (scroll_start > offset.top) {
+                $('.fa-book').css('color', '#337ab7');
+                $('.fa-book').css('transition', '0.5s');
+            } else {
+                $(".fa-book").css('color', '#F58900');
+                $('.fa-book').css('transition', '0.5s');
+            }
+        });
+    }
+});
